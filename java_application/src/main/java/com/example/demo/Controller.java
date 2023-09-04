7bfc02cedb05ed42a946754e42669dc946a7e4a2
tommoy-chae <tommoy.chae@kakaoenterprise.com>
2023-09-04 17:58:23 +0900

    임커

--- java_application/src/main/java/com/example/demo/Controller.java
+++ java_application/src/main/java/com/example/demo/Controller.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 
 import java.util.logging.Logger;
 
@@ -33,6 +38,16 @@ public class Controller {
     public ResponseEntity<String> SendGolangDBAPI() {
         ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/golang/db", String.class);
         return response;
+    }
+
+    @GetMapping("/dynamodb")
+    public Integer DynamoDB() {
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "ap-northeast-2");
+
+        AmazonDynamoDB amazonDynamoDb = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(endpointConfiguration).build();;
+        ScanResult result = amazonDynamoDb.scan(new ScanRequest("tommoy_test"));
+        return result.getCount();
     }
 
     private void MyFunction1(String arg1) {
